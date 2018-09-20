86baae65cb2d209be187732a37c148382a377ac3
muderiz <42927576+muderiz@users.noreply.github.com>
2018-09-20 15:51:45 +0700

    Test Perkalian Looping

--- src/Looping.java
+++ src/Looping.java
@@ -5,19 +5,24 @@ class Looping {
     public static void main(String[] args) {
         System.out.println(loop(-3, -4));
     }
-/**
- * Dokumentasi
- * @param a menampung data integer
- * @param b menampung data integer
- * @return untuk menyimpan hasil perkalian
- */
+
+    /**
+     * Dokumentasi
+     *
+     * @param a menampung data integer
+     * @param b menampung data integer
+     * @return untuk menyimpan hasil perkalian
+     */
     public static int loop(int a, int b) {
         int jumlah = 0;
         if (a < 0 && b < 0);
-            int c = a * -1, d = b * -1;            
+        int c = a * -1, d = b * -1;
         for (int i = b; i < 0; i++) {
             jumlah = c * d;
         }
         return jumlah;
+        
+        
+        
     }
 }
