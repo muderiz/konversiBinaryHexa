4c4055748b591bb9f6a11e2c5ac739ead8987f22
muderiz <42927576+muderiz@users.noreply.github.com>
2018-09-20 15:36:09 +0700

    Test Perkalian Looping

---
+++ src/Looping.java
@@ -0,0 +1,28 @@
+//import static java.lang.System.out;
+
+class Looping {
+
+    public static void main(String[] args) {
+          int i=0;
+          do{
+            System.out.println(i);
+            i++;} while (i<10);
+        }
+        //System.out.println(loop(-3, -4));
+    }
+/**
+ * Dokumentasi
+ * @param a menampung data integer
+ * @param b menampung data integer
+ * @return untuk menyimpan hasil perkalian
+ */
+    public static int loop(int a, int b) {
+        int jumlah = 0;
+        if (a < 0 && b < 0);
+            int c = a * -1, d = b * -1;            
+        for (int i = b; i < 0; i++) {
+            jumlah = c * d;
+        }
+        return jumlah;
+    }
+}
---
+++ src/Perkalian.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Perkalian {
+    public static void main(String[] args) {
+        Scanner masuk = new Scanner(System.in);
+        System.out.print("NILAI PERTAMA:");
+        int N1 = masuk.nextInt();
+        System.out.print("NILAI KEDUA:");
+        int N2 = masuk.nextInt();
+        int hasil = N1 * N2;
+        System.out.print("hasil total=" + hasil);
+
+            
+        
+    }
+
+}
